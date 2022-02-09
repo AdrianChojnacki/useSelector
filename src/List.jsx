be432e3d44bda7182556e4a12acdf6dff2e12bdd
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-02-09 21:46:14 +0100

    useSelector

--- src/List.jsx
+++ src/List.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Element from './Element';
 
-const List = ({ ratesInProps }) => {
-  const ratesElements = ratesInProps.map(rate => (
+const List = () => {
+  const rates = useSelector(store => store.rates);
+  const ratesElements = rates.map(rate => (
     <Element key={rate.id} {...rate} />
   ));
 
@@ -15,10 +16,4 @@ const List = ({ ratesInProps }) => {
   );
 };
 
-const connectReduxStateToProps = store => ({
-  ratesInProps: store.ratesFromReducer,
-});
-
-const ListConsumer = connect(connectReduxStateToProps)(List);
-
-export default ListConsumer;
+export default List;
